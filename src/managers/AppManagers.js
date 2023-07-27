949bf990bfb6b88d326cd38ea565178a5812ead1
eliranr <eliranr497@gmail.com>
2023-07-27 16:55:29 +0300

    axios2

--- src/managers/AppManagers.js
+++ src/managers/AppManagers.js
@@ -74,7 +74,7 @@ function App() {
   }, [currentLang]);
 
   useEffect(() => {
-    axios.get(`https://test-eight-sigma-86.vercel.app/get-data`)
+    axios.get(`https://jsonplaceholder.typicode.com/users`)
       .then(res => {
         console.log(res.data);
     })
